99243580fb4e4894289afbbf28c260945b49144a
Gustavo <gusdelug@hotmail.com>
2022-03-28 09:51:10 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from time import sleep
+from machine import Pin, PWM
+import network
+from network import WLAN
+from mqtt import MQTTClient
+def sub_callback(topic, msg): 
+    if topic.decode("utf-8")=="espriella/feeds/servoduty":
+        servo.duty(int(msg))
+        sleep(2)
+        servo.duty(0)
+    print(topic, msg)
+def settimeout(duration): 
+    pass
+
+wlan = WLAN(network.STA_IF)
+if not wlan.isconnected():
+    wlan.active(True)
+    wlan.connect("hydra", "elbuengus")
+    while not wlan.isconnected(): 
+        pass
+print("Connected to Wifi", wlan.ifconfig())
+
+client = MQTTClient("LOLIN32", "io.adafruit.com",user="espriella", password="aio_DSVF93S1wRcTogplHexUV16ep9rr", port=1883)
+client.settimeout = settimeout
+client.set_callback(sub_callback)
+client.connect()
+client.subscribe(topic="espriella/feeds/servoduty")
+servo = PWM(Pin(23))
+servo.freq(50)
+while True:
+    client.check_msg()
